73be92a9bc654d06b602ad9e8069f6dd85067e7f
Erwin Heitzman <erwin.heitzman@gmail.com>
2017-07-16 10:32:37 +0200

    removed xml from js files

--- test/dummyTestFiles/evenMoreTests/example.js
+++ test/dummyTestFiles/evenMoreTests/example.js
@@ -1,18 +1,3 @@
-<testsuites>
-    <testsuite tests="3">
-        <testcase classname="foo1" name="ASuccessfulTest"/>
-        <testcase classname="foo2" name="AnotherSuccessfulTest"/>
-        <testcase classname="foo3" name="AFailingTest">
-            <failure type="NotEnoughFoo"> details about failure </failure>
-        </testcase>
-        <testcase classname="foo3" name="AFailingTest">
-            <failure type="NotEnoughFoo"> details about failure </failure>
-        </testcase>
-        <testcase classname="foo89" name="21598 - AFailingTest">
-            <failure type="NotEnoughFoozst"> details about failure </failure>
-        </testcase>
-    </testsuite>
-</testsuites>
 describe('13849 - SDFSDFSDF', function () {
     const testDecleration = () => '21598 - AFailingTest as an example'
 
@@ -98,18 +83,3 @@ describe('13849 - SDFSDFSDF', function () {
         })
     })
 });
-<testcase classname="foo89" name="21598 - 4545435434"/>
-"DATE:20091201T220000\r\nSUMMARY:Dad's birthday"
-'DATE:20091201T220000\r\nSUMMARY:Dad\'s birthday'
-y"
-'DATE:20091201T220000\r\nSUMMARY:Dad\'s birthday'
-y"
-'DATE:20091201T220000\r\nSUMMARY:Dad\'s birthday'
-y"
-'DATE:20091201T220000\r\nSUMMARY:Dad\'s birthday'
-y"
-'DATE:20091201T220000\r\nSUMMARY:Dad\'s birthday'
-y"
-'DATE:20091201T220000\r\nSUMMARY:Dad\'s birthday'
-y"
-'DATE:20091201T220000\r\nSUMMARY:Dad\'s birthday'
