157762df574f12b640997bc0384d3d9465f6217e
Erwin Heitzman <erwin.heitzman@gmail.com>
2017-07-16 02:23:18 +0200

    removed code, get/set methods of ConfigParser are now static

--- test/ConfigParser.test.js
+++ test/ConfigParser.test.js
@@ -15,43 +15,43 @@ describe('ConfigParser.config', () => {
     const jsConfig = require('../rapid.conf.js')()
 
     it('should return DEFAULT_CONFIG when no config is set', () => {
-        expect(configParser.config).to.deep.equal(DEFAULT_CONFIG)
+        expect(ConfigParser.config).to.deep.equal(DEFAULT_CONFIG)
     })
 
     it('should set the given config to be used', () => {
-        configParser.config = 'rapid.json'
+        ConfigParser.config = 'rapid.json'
 
-        expect(configParser.config).to.deep.equal(jsonConfig)
+        expect(ConfigParser.config).to.deep.equal(jsonConfig)
     })
 
     it('should support json configs', () => {
-        configParser.config = 'rapid.json'
+        ConfigParser.config = 'rapid.json'
 
-        expect(configParser.config).to.deep.equal(jsonConfig)
+        expect(ConfigParser.config).to.deep.equal(jsonConfig)
     })
 
     it('should support js configs', () => {
-        configParser.config = 'rapid.conf.js'
+        ConfigParser.config = 'rapid.conf.js'
 
-        expect(configParser.config).to.deep.equal(jsConfig)
+        expect(ConfigParser.config).to.deep.equal(jsConfig)
     })
 
     it('should support configs that return an object', () => {
-        configParser.config = 'rapid.json'
+        ConfigParser.config = 'rapid.json'
 
-        expect(configParser.config).to.deep.equal(jsonConfig)
+        expect(ConfigParser.config).to.deep.equal(jsonConfig)
     })
 
     it('should support configs that return a function', () => {
-        configParser.config = 'rapid.conf.js'
+        ConfigParser.config = 'rapid.conf.js'
 
-        expect(configParser.config).to.deep.equal(jsConfig)
+        expect(ConfigParser.config).to.deep.equal(jsConfig)
     })
 
     it('should support merging configurations with the DEFAULT_CONFIG', () => {
         const mergedConfig = Object.assign(DEFAULT_CONFIG, jsConfig)
         configParser.merge = 'rapid.conf.js'
 
-        expect(configParser.config).to.deep.equal(mergedConfig)
+        expect(ConfigParser.config).to.deep.equal(mergedConfig)
     })
 })
