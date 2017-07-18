e9c0c633d2cf03db573dbf8dddcbae2680019b1c
Erwin Heitzman <erwin.heitzman@gmail.com>
2017-07-18 22:34:30 +0200

    added json files for later use, adjusted test accordingly

--- test/Files.test.js
+++ test/Files.test.js
@@ -11,22 +11,38 @@ describe('Files.get', () => {
         path.join(__dirname, '..', 'results\\results_03.xml'),
         path.join(__dirname, '..', 'results\\results_04.xml'),
     ]
+    const jsonFiles = [
+        path.join(__dirname, '..', 'results\\results_01.json'),
+        path.join(__dirname, '..', 'results\\results_02.json'),
+        path.join(__dirname, '..', 'results\\results_03.json'),
+        path.join(__dirname, '..', 'results\\results_04.json'),
+    ]
     const jsFiles = [
         path.join(__dirname, '..', 'results\\dummyTestFiles\\evenMoreTests\\example.js'),
         path.join(__dirname, '..', 'results\\dummyTestFiles\\moreTests\\andMore\\tests.js'),
         path.join(__dirname, '..', 'results\\dummyTestFiles\\moreTests\\andMore\\tests2.js'),
     ]
+    const allFilesInRootFolder = [
+        path.join(__dirname, '..', 'results\\results_01.json'),
+        path.join(__dirname, '..', 'results\\results_01.xml'),
+        path.join(__dirname, '..', 'results\\results_02.json'),    
+        path.join(__dirname, '..', 'results\\results_02.xml'),
+        path.join(__dirname, '..', 'results\\results_03.json'),
+        path.join(__dirname, '..', 'results\\results_03.xml'),
+        path.join(__dirname, '..', 'results\\results_04.json'),
+        path.join(__dirname, '..', 'results\\results_04.xml'),
+    ]
 
     it('should return a array of all files from the given directory', () => {
         const actual = files.get(absolutePath)
 
-        expect(actual).to.deep.equal(xmlFiles)
+        expect(actual).to.deep.equal(allFilesInRootFolder)
     })
 
     it('should return a array of all files from the given directory and all sub directories', () => {
         const actual = files.get(absolutePath, [], true)
 
-        expect(actual).to.deep.equal(jsFiles.concat(xmlFiles))
+        expect(actual).to.deep.equal(jsFiles.concat(allFilesInRootFolder))
     })
 
     it('should return a array of all xml files from the given directory', () => {
