d5e94c3f4875ca74b3c3142a380ce07af8f8dec3
Erwin Heitzman <erwin.heitzman@gmail.com>
2017-07-19 01:18:04 +0200

    fix references to ConfigParser and add config filename/path

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-// const Tests = require('./lib/Tests').Tests
-// const tests = new Tests
-// tests.disable()
-const path = require('path')
-const configPath = path.resolve(__dirname, './_config.json')
-
-const ConfigParser = require('./lib/utils/ConfigParser').ConfigParser
-const configParser = new ConfigParser
-configParser.addConfig(configPath)
-
-console.log(configParser.config)
+const Tests = require('./lib/Tests').Tests
+const tests = new Tests
+tests.disable()
