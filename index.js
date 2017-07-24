43b0d0070f886384ea332d3375bd7ea9de8342e8
Erwin Heitzman <erwin.heitzman@gmail.com>
2017-07-24 20:09:01 +0200

    removed tmp folder, set url

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 const Githandler = require('./lib/utils/SourceControlHandler.js')
 const gitHandler = new Githandler
-gitHandler.clone('https://github.com/erwinheitzman/selenium-build-monitor.git')
+gitHandler.clone()
