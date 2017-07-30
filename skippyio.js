2202fbd5a9e8d22f20ff284af0bbbb56aea369ab
Erwin Heitzman <erwin.heitzman@gmail.com>
2017-07-30 12:54:27 +0200

    fixed bug causing process not to close, fixed bug that caused the process to continue where it should have paused

--- skippyio.js
+++ skippyio.js
@@ -1,7 +1,7 @@
 const configurationHelper = require('./lib/utils/ConfigurationHelper.js').ConfigurationHelper
-configurationHelper.init()
+configurationHelper.init(() => {
+    const Githandler = require('./lib/utils/SourceControlHandler.js')
+    const gitHandler = new Githandler
 
-const Githandler = require('./lib/utils/SourceControlHandler.js')
-const gitHandler = new Githandler
-
-gitHandler.disableTestsAndPushChanges()
+    gitHandler.disableTestsAndPushChanges()
+})
