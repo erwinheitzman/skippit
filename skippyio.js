e36e01b075269c55e2df25fbdf42661173741938
Erwin Heitzman <erwin.heitzman@gmail.com>
2017-07-30 11:37:29 +0200

    added ConfigurationHelper, made some small other changes

--- skippyio.js
+++ skippyio.js
@@ -1,7 +1,7 @@
-// require('./lib/utils/ConfigurationHelper.js')
-// const ConfigurationHelper = require('./lib/utils/ConfigurationHelper.js').ConfigurationHelper
-// const configurationHelper = new ConfigurationHelper
-// configurationHelper.init()
+const configurationHelper = require('./lib/utils/ConfigurationHelper.js').ConfigurationHelper
+configurationHelper.init()
+
 const Githandler = require('./lib/utils/SourceControlHandler.js')
 const gitHandler = new Githandler
+
 gitHandler.disableTestsAndPushChanges()
