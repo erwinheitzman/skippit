c22abc3f7c8c55a763cf98689b43f8e46e9975ba
Erwin Heitzman <erwin.heitzman@gmail.com>
2019-08-30 00:17:42 +0200

    simplyfied configs

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,4 @@
-// For a detailed explanation regarding each configuration property, visit:
-// https://jestjs.io/docs/en/configuration.html
-
 module.exports = {
-	clearMocks: true,
 	collectCoverage: true,
 	coverageDirectory: "coverage",
 	coverageReporters: [
@@ -17,42 +13,11 @@ module.exports = {
 			statements: 100
 		},
 	},
-
 	transform: {
 		'^.+\\.ts$': 'ts-jest',
 	},
-
-	// A path to a module which exports an async function that is triggered once before all test suites
-	// globalSetup: null,
-
-	// A path to a module which exports an async function that is triggered once after all test suites
-	// globalTeardown: null,
-
-	// Use this configuration option to add custom reporters to Jest
-	// reporters: undefined,
-
-	// Automatically reset mock state between every test
-	// resetMocks: false,
-
-	// Reset the module registry before running each individual test
-	// resetModules: false,
-
 	roots: [
 		"src"
 	],
-
-	// The paths to modules that run some code to configure or set up the testing environment before each test
-	// setupFiles: [],
-
-	// A list of paths to modules that run some code to configure or set up the testing framework before each test
-	// setupFilesAfterEnv: [],
-
-	// A list of paths to snapshot serializer modules Jest should use for snapshot testing
-	// snapshotSerializers: [],
-
-	// The test environment that will be used for testing
 	testEnvironment: "node",
-
-	// Options that will be passed to the testEnvironment
-	// testEnvironmentOptions: {},
 };
